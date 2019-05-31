3f3de2c9a7381ffb5b7a2e0ed6c38865471b2e57
Giorgio Natale Buttiglieri <10623930@polimi.it>
2019-05-31 04:47:51 +0200

    Updating task 3, and the first two creative tasks

--- creative_task_1.py
+++ creative_task_1.py
@@ -13,6 +13,7 @@ n_packets = capinfos("network_traffic.pcap")["packetscount"]
 widgets = ['SNIFFING: ', progressbar.Percentage(), progressbar.Bar(), progressbar.SimpleProgress(), ' - ', progressbar.Timer(), ', ', progressbar.ETA()]
 bar = progressbar.ProgressBar(widgets = widgets, max_value=n_packets)
 
+#_conn is a dictionary that holds every tcp conversation (ip tuple -> list of packets)
 def GetConnections(_counter, _conn):
     def elaborate(_pkt):
         _counter[0] += 1
@@ -32,17 +33,22 @@ sniff(offline="network_traffic.pcap", store = 0, prn=GetConnections(counter, con
 bar.finish()
 print("Sniffing finished")
 
-#Counting amount of repeated ack for each connection 
+#dictionary that contains tuples of ips with corresponding errors 
 conn_errors = {}
+
+#Counting amount of repeated ack for each connection
 for conn_ips, pkts in conn.items():
+    #dictionary that associate the ack number to the number of times that ack has been resent
     n_ack = {}
     for p in pkts:
         ACK_BITMASK = 0x10 
+        #if we have a valid ACK flag
         if p[TCP].flags & ACK_BITMASK:
             if not p[TCP].ack in n_ack:
                 n_ack[p[TCP].ack] = 0
             n_ack[p[TCP].ack] += 1
 
+    #we consider an error in the connection if more than 3 ack are resent
     for ack, ack_counter in n_ack.items():
         if ack_counter > 3:
             if not conn_ips in conn_errors:
@@ -50,7 +56,9 @@ for conn_ips, pkts in conn.items():
             conn_errors[conn_ips] += 1
 
 
+#this dictionary merge the two directional channels of connections. Every tuple of ip is associated with one and only one conversation
 bidirectional_conn_errors = {}
+
 for conn_ips, n_errors in conn_errors.items():
     if conn_ips in bidirectional_conn_errors: 
         bidirectional_conn_errors[conn_ips] += n_errors
@@ -59,6 +67,7 @@ for conn_ips, n_errors in conn_errors.items():
     else:
         bidirectional_conn_errors[conn_ips] = n_errors
 
+#finding the biggest value for errors (it will be important for the line color)
 max_errors_conn = -1
 for conn_ips, n_errors in bidirectional_conn_errors.items():
     if n_errors > max_errors_conn:
@@ -69,8 +78,8 @@ for conn_ips, n_errors in bidirectional_conn_errors.items():
 ip_loc_df = pd.read_csv('real.csv')
 ip_loc_df = ip_loc_df.set_index('ip')
 
-index = pd.MultiIndex.from_tuples(list(bidirectional_conn_errors.keys()), names=['sender', 'receiver'])
-conn_df = pd.DataFrame(list(bidirectional_conn_errors.values()), columns=['repetitions'], index = index)
+index = pd.MultiIndex.from_tuples(list(bidirectional_conn_errors.keys()), names=['ip1', 'ip2'])
+conn_df = pd.DataFrame(list(bidirectional_conn_errors.values()), columns=['errors'], index = index)
 
 lines = []
 for row in conn_df.index.tolist():
@@ -85,40 +94,28 @@ for row in conn_df.index.tolist():
 
 
 
-#printing the map
+#loading the map
 world_map = gpd.read_file('TM_WORLD_BORDERS-0.3.shp')
+
+#setting coordinates type
 crs = {'init':'epsg:4326'}
+
 fig, ax = plt.subplots(figsize = (15, 15))
 
-#a = 0
-#for ips, n in bidirectional_conn_errors.items():
-#    if a == 0:
-#        a = len(conn_df.loc[ips[0], ips[1]])
-#        print(a)
-#    if len(conn_df.loc[ips[0], ips[1]]) != a:
-#        print(ips, len(conn_df.loc[ips[0], ips[1]]))
-#print('ok')
-
-
+#creating a geo dataframe with information on the lines to plot
 geo_df = gpd.GeoDataFrame(conn_df, crs = crs, geometry = lines)
 world_map.plot(ax = ax)
 
 
-#Solution row by row !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-
+#plot lines with color rapresenting the quality of the connection
 for ips, n in bidirectional_conn_errors.items():
     if n < 3:
         _color = (n/2, (2-n)/2, 0, 0.85)
     else:
         _color = (1-(0.4*n+0.6*max_errors_conn-3)/(max_errors_conn-3),(0.4*n+0.6*max_errors_conn-3)/(max_errors_conn-3),0,0.95)
     geo_df.loc[[ips[0], ips[1]], 'geometry'].plot(ax = ax, color = _color)
-print(geo_df)
 plt.show()
 
-
-#ehyasdf
-#khgdhjskdjlkjdwkà
-#wjklhjdkl
 
 
 
--- task5.py
+++ task5.py
@@ -4,15 +4,25 @@ import descartes
 import geopandas as gpd
 from shapely.geometry import Point, Polygon
 
+#loading the map
 world_map = gpd.read_file('TM_WORLD_BORDERS-0.3.shp')
+
+#reading ip location
 ip_data = pd.read_csv('real.csv')
+
+#setting coordinates type
 crs = {'init':'epsg:4326'}
 
+#creating a list of points with coordinates given by the ip locations
 points = [Point(xy) for xy in zip(ip_data["y"], ip_data["x"])]
+
+
 fig, ax = plt.subplots(figsize = (15, 15))
 
+#creating the geopandas dataframe with proper geometry column
 geo_df = gpd.GeoDataFrame(ip_data, crs = crs, geometry = points)
 
+#plot everything
 world_map.plot(ax = ax)
 geo_df.plot(ax = ax, markersize = 0.1, color = "red", marker = "o")
 plt.show()
